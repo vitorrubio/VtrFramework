65750f73f86e293a312e13437831aa567f20ebf2
Vitor Luiz Rubio <vitorrubio@gmail.com>
2017-01-05 17:42:32 -0200

    alguns testes foram feitos, e agora vai rolar uma limpeza

--- VtrFramework.Test/OperatorOverloadTest.cs
+++ VtrFramework.Test/OperatorOverloadTest.cs
@@ -11,7 +11,6 @@ namespace VtrFramework.Test
     [TestFixture]
     public class AllTypecastsOperatorOverloadTest
     {
-
         [TestFixture]
         public class ImplicitInt32OperatorOverloadTest
         {
@@ -331,68 +330,68 @@ namespace VtrFramework.Test
 
 
 
-        //[TestFixture]
-        //public class ImplicitUInt64OperatorOverloadTest
-        //{
-        //    [Test]
-        //    public void EqualsOperatorTest()
-        //    {
-        //        Foo f1 = new Foo(1);
-        //        UInt64 i1 = 1;
-        //        UInt64 i2 = 2;
+        [TestFixture]
+        public class ImplicitUInt64OperatorOverloadTest
+        {
+            [Test]
+            public void EqualsOperatorTest()
+            {
+                Foo f1 = new Foo(1);
+                UInt64 i1 = 1;
+                UInt64 i2 = 2;
 
-        //        Console.WriteLine("f1 == i1");
-        //        Assert.IsTrue(f1 == i1);
-        //        Console.WriteLine("i1 == f1");
-        //        Assert.IsTrue(i1 == f1);
+                Console.WriteLine("f1 == i1");
+                Assert.IsTrue(f1 == i1);
+                Console.WriteLine("i1 == f1");
+                Assert.IsTrue(i1 == f1);
 
-        //        Console.WriteLine("f1 == 1");
-        //        Assert.IsTrue(f1 == 1);
-        //        Console.WriteLine("1 == f1");
-        //        Assert.IsTrue(1 == f1);
-
-
-        //        Console.WriteLine("f1 == i2");
-        //        Assert.IsFalse(f1 == i2);
-        //        Console.WriteLine("i2 == f1");
-        //        Assert.IsFalse(i2 == f1);
-
-        //        Console.WriteLine("f1 == 2");
-        //        Assert.IsFalse(f1 == 2);
-        //        Console.WriteLine("2 == f1");
-        //        Assert.IsFalse(2 == f1);
-        //    }
+                Console.WriteLine("f1 == 1");
+                Assert.IsTrue(f1 == 1);
+                Console.WriteLine("1 == f1");
+                Assert.IsTrue(1 == f1);
 
 
-        //    [Test]
-        //    public void UnequalsOperatorTest()
-        //    {
-        //        Foo f1 = new Foo(1);
-        //        UInt64 i1 = 1;
-        //        UInt64 i2 = 2;
+                Console.WriteLine("f1 == i2");
+                Assert.IsFalse(f1 == i2);
+                Console.WriteLine("i2 == f1");
+                Assert.IsFalse(i2 == f1);
 
-        //        Console.WriteLine("f1 != i1");
-        //        Assert.IsFalse(f1 != i1);
-        //        Console.WriteLine("i1 != f1");
-        //        Assert.IsFalse(i1 != f1);
-
-        //        Console.WriteLine("f1 != 1");
-        //        Assert.IsFalse(f1 != 1);
-        //        Console.WriteLine("1 != f1");
-        //        Assert.IsFalse(1 != f1);
+                Console.WriteLine("f1 == 2");
+                Assert.IsFalse(f1 == 2);
+                Console.WriteLine("2 == f1");
+                Assert.IsFalse(2 == f1);
+            }
 
 
-        //        Console.WriteLine("f1 != i2");
-        //        Assert.IsTrue(f1 != i2);
-        //        Console.WriteLine("i2 != f1");
-        //        Assert.IsTrue(i2 != f1);
+            [Test]
+            public void UnequalsOperatorTest()
+            {
+                Foo f1 = new Foo(1);
+                UInt64 i1 = 1;
+                UInt64 i2 = 2;
 
-        //        Console.WriteLine("f1 != 2");
-        //        Assert.IsTrue(f1 != 2);
-        //        Console.WriteLine("2 != f1");
-        //        Assert.IsTrue(2 != f1);
-        //    }
-        //}
+                Console.WriteLine("f1 != i1");
+                Assert.IsFalse(f1 != i1);
+                Console.WriteLine("i1 != f1");
+                Assert.IsFalse(i1 != f1);
+
+                Console.WriteLine("f1 != 1");
+                Assert.IsFalse(f1 != 1);
+                Console.WriteLine("1 != f1");
+                Assert.IsFalse(1 != f1);
+
+
+                Console.WriteLine("f1 != i2");
+                Assert.IsTrue(f1 != i2);
+                Console.WriteLine("i2 != f1");
+                Assert.IsTrue(i2 != f1);
+
+                Console.WriteLine("f1 != 2");
+                Assert.IsTrue(f1 != 2);
+                Console.WriteLine("2 != f1");
+                Assert.IsTrue(2 != f1);
+            }
+        }
 
 
         [TestFixture]
@@ -759,6 +758,9 @@ namespace VtrFramework.Test
         //https://msdn.microsoft.com/en-us/library/85w54y0a.aspx
         //https://msdn.microsoft.com/en-us/library/ms229033.aspx
         //https://msdn.microsoft.com/en-us/library/ms173147.aspx
+        //https://msdn.microsoft.com/en-us/library/aa664464(v=vs.71)
+        //https://msdn.microsoft.com/en-us/library/aa288467(v=vs.71).aspx
+        //https://msdn.microsoft.com/en-us/library/ms229032.aspx
 
 
         private Int32 _value = 0;
@@ -800,14 +802,514 @@ namespace VtrFramework.Test
         public static implicit operator Foo(Int32 i)
         {
             Console.WriteLine("A cast Int32 -> Foo ocurred");
-            return new Foo(i);
+            return new Foo((Int32)i);
         }
 
         public static implicit operator Int32(Foo f)
         {
             Console.WriteLine("A cast Foo -> Int32 ocurred");
-            return f != null ? f.Value : (Int32)0;
+            return f != null ? (Int32)f.Value : (Int32)0;
         }
+
+
+        public static bool operator == (Foo x, Foo y)
+        {
+            Console.WriteLine("A Foo == Foo ocurred");
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, Foo y)
+        {
+            Console.WriteLine("A Foo != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        #region uint64
+
+
+        public static bool operator ==(UInt64 x, Foo y)
+        {
+            Console.WriteLine("A Uint64 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(UInt64 x, Foo y)
+        {
+            Console.WriteLine("A Uint64 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, UInt64 y)
+        {
+            Console.WriteLine("A Foo == UInt64 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, UInt64 y)
+        {
+            Console.WriteLine("A Foo != Uint64 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region int64
+
+
+        public static bool operator ==(Int64 x, Foo y)
+        {
+            Console.WriteLine("A Int64 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(Int64 x, Foo y)
+        {
+            Console.WriteLine("A Int64 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, Int64 y)
+        {
+            Console.WriteLine("A Foo == Int64 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, Int64 y)
+        {
+            Console.WriteLine("A Foo != Int64 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region uint32
+
+
+        public static bool operator ==(UInt32 x, Foo y)
+        {
+            Console.WriteLine("A UInt32 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(UInt32 x, Foo y)
+        {
+            Console.WriteLine("A UInt32 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, UInt32 y)
+        {
+            Console.WriteLine("A Foo == UInt32 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, UInt32 y)
+        {
+            Console.WriteLine("A Foo != UInt32 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region int32
+
+
+        public static bool operator ==(Int32 x, Foo y)
+        {
+            Console.WriteLine("A Int32 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(Int32 x, Foo y)
+        {
+            Console.WriteLine("A Int32 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, Int32 y)
+        {
+            Console.WriteLine("A Foo == Int32 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, Int32 y)
+        {
+            Console.WriteLine("A Foo != Int32 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region uint16
+
+
+        public static bool operator ==(UInt16 x, Foo y)
+        {
+            Console.WriteLine("A UInt16 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(UInt16 x, Foo y)
+        {
+            Console.WriteLine("A UInt16 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, UInt16 y)
+        {
+            Console.WriteLine("A Foo == UInt16 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, UInt16 y)
+        {
+            Console.WriteLine("A Foo != UInt16 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region Int16
+
+
+        public static bool operator ==(Int16 x, Foo y)
+        {
+            Console.WriteLine("A Int16 == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(Int16 x, Foo y)
+        {
+            Console.WriteLine("A Int16 != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, Int16 y)
+        {
+            Console.WriteLine("A Foo == Int16 ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, Int16 y)
+        {
+            Console.WriteLine("A Foo != Int16 ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region SByte
+
+
+        public static bool operator ==(SByte x, Foo y)
+        {
+            Console.WriteLine("A SByte == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(SByte x, Foo y)
+        {
+            Console.WriteLine("A SByte != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, SByte y)
+        {
+            Console.WriteLine("A Foo == SByte ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, SByte y)
+        {
+            Console.WriteLine("A Foo != SByte ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
+
+        #region Byte
+
+
+        public static bool operator ==(Byte x, Foo y)
+        {
+            Console.WriteLine("A Byte == Foo ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y.Value);
+
+        }
+
+
+        public static bool operator !=(Byte x, Foo y)
+        {
+            Console.WriteLine("A Byte != Foo ocurred");
+            return !(x == y);
+        }
+
+
+
+
+        public static bool operator ==(Foo x, Byte y)
+        {
+            Console.WriteLine("A Foo == Byte ocurred");
+
+            if (((object)x == null) && ((object)y == null))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Value.Equals(y);
+
+        }
+
+
+        public static bool operator !=(Foo x, Byte y)
+        {
+            Console.WriteLine("A Foo != Byte ocurred");
+            return !(x == y);
+        }
+
+
+        #endregion
 
 
     }
